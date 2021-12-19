d9c9accf764c16c8698f92377ee94844f268f889
gtrman97 <andrew.killian@g.austincc.edu>
2021-12-19 13:05:37 -0600

    added function definitions

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 
 function displayEasy() {
 
+    document.getElementById("container1").style.display = "none"; 
+    document.getElementById("container2").style.display = "none"; 
+    document.getElementById("container3").style.display = "grid";
+
 }
 
 function displayMedium() {
 
     document.getElementById("container1").style.display = "none"; 
-    document.getElementById("container3").style.display = "none"; 
     document.getElementById("container2").style.display = "grid"; 
-
+    document.getElementById("container3").style.display = "none"; 
 
 }
 
 function displayHard() {
+
+    document.getElementById("container1").style.display = "none"; 
+    document.getElementById("container2").style.display = "none"; 
+    document.getElementById("container3").style.display = "grid"; 
 
 }
