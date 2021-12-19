59c82b291ef041a925d960d63f12fe801df1c6c1
gtrman97 <andrew.killian@g.austincc.edu>
2021-12-19 13:04:48 -0600

    added function call

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ function displayEasy() {
 
 function displayMedium() {
 
+    document.getElementById("container1").style.display = "none"; 
+    document.getElementById("container3").style.display = "none"; 
+    document.getElementById("container2").style.display = "grid"; 
+
+
 }
 
 function displayHard() {
-    
+
 }
